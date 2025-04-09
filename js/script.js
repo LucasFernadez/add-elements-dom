1072fcdf0406b66545193f4b1d40ee23c196b487
Lucas <lucasway2000@yahoo.es>
2025-04-09 20:44:27 +0200

    final

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
  // Aquí tu código
+const boton = document.getElementById('agregarBtn');
+const lista = document.getElementById('lista');
+
+boton.addEventListener('click', () => {
+
+  const nuevoElemento = prompt('Introduce un nuevo elemento:');
+  if (nuevoElemento && nuevoElemento.trim() !== '') {
+    const li = document.createElement('li');
+    li.textContent = nuevoElemento; 
+    lista.appendChild(li);
+  }
+});
